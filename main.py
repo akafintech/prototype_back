a571b837f9f5798f577773da73e079d7b53b1210
miyaojin0323 <miyaojin0323@gmail.com>
2025-07-31 23:14:54 +0900

    cors 허용 origin 수정

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Lemong Backend API", version="1.0.0",docs_url="/docs", redo
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # 프론트엔드 주소
+    allow_origins=["*"],  # 프론트엔드 주소
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
