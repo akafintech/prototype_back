ee6b925b81b06c9d7a0e8a790853ef338c65d26b
miyaojin0323 <miyaojin0323@gmail.com>
2025-08-01 01:12:35 +0900

    영어 번역 기능 추가

--- app/controller/recommend.py
+++ app/controller/recommend.py
@@ -23,10 +23,12 @@ def get_reviews(recommend:RecommendCreate,
     storename = recommend.storename
 
     long_short = "short" if rating > 4 else "long"
+    index = random.randint(0,4)
 
-    casual_text = random.choice(recommend_data["casual"][long_short])
-    formal_text = random.choice(recommend_data["formal"][long_short])
-    business_text = random.choice(recommend_data["business"][long_short])
+    # 한글
+    casual_text = recommend_data["casual"][long_short][index]
+    formal_text = recommend_data["formal"][long_short][index]
+    business_text = recommend_data["business"][long_short][index]
 
     casual_text = casual_text.replace("00님", f"{username}님")
     formal_text = formal_text.replace("00님", f"{username}님")
@@ -36,8 +38,28 @@ def get_reviews(recommend:RecommendCreate,
     formal_text = formal_text.replace("00호텔", storename)
     business_text = business_text.replace("00호텔", storename)
 
-    return {"results": [
-        {"id":1,"title": "간결한 버전", "text": casual_text},
-        {"id":2,"title": "공손한 버전", "text": formal_text},
-        {"id":3,"title": "친근한 버전", "text": business_text }
-    ]}
+    #영어
+    casual_en_text = recommend_data["casual_en"][long_short][index]
+    formal_en_text = recommend_data["formal_en"][long_short][index]
+    business_en_text = recommend_data["business_en"][long_short][index]
+
+    casual_en_text = casual_en_text.replace("00,", f"{username},")
+    formal_en_text = formal_en_text.replace("00,", f"{username},")
+    business_en_text = business_en_text.replace("00,", f"{username},")
+
+    casual_en_text = casual_en_text.replace("00,", f"{username},")
+    formal_en_text = formal_en_text.replace("00,", f"{username},")
+    business_en_text = business_en_text.replace("00,", f"{username},")
+
+    return {
+        "results": [
+            {"id":1,"title": "간결한 버전", "text": casual_text},
+            {"id":2,"title": "공손한 버전", "text": formal_text},
+            {"id":3,"title": "친근한 버전", "text": business_text}
+        ],
+        "results_en":[
+            {"id":1,"title": "casual version", "text": casual_en_text},
+            {"id":2,"title": "formal version", "text": formal_en_text},
+            {"id":3,"title": "business casual version", "text": business_en_text}
+        ]
+    }
